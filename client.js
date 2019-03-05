006c293f99f0d6b1b871daec14336bab83c8ea14
Nikolas Markides <nikolasmarkides@live.com>
2019-03-05 23:21:19 +0000

    website v2 layout
    Website small layout change, section divider(left api, right html).  Added html time widget.

---
+++ client.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// Client to serve the apis to the server:
+//server functions:
+
+
+//SPOTIFY:
+
+//BLOGGER:
--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + 'hello');
 });
-
 //start the server
 app.listen(8080, (err) =>{
     if(err) console.log('error starting the server', err);
@@ -19,20 +18,8 @@ app.listen(8080, (err) =>{
 })
 
 
-//get info on sign in:
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+function error(res, msg) {
+  res.sendStatus(500);
+  console.error(msg);
 }
-//function to sign out:
-
-
-
-
-app.get("/time", function (req,res){
-  res.status(200).send(func.getTime());
-  res.end();
-});
