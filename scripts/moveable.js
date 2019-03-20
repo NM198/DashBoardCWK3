0e27db0406e85737c700a85351dca49cf6cb698b
Nikolas Markides <nikolasmarkides@live.com>
2019-03-20 23:26:43 +0000

    added the settings tab, fixed draggable items
    Fixed draggable items to have a default position.  Added the edit tab with the ability to change backround changed home icon.  Fixed footer to stay at bottom of page.

--- scripts/moveable.js
+++ scripts/moveable.js
@@ -2,6 +2,8 @@
 dragElement(document.getElementById("clock"));
 dragElement(document.getElementById("calendar"));
 
+
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
@@ -40,5 +42,42 @@ function dragElement(elmnt) {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
+  }
+}
+document.getElementById("backbutton").addEventListener("click", function(){
+    //write funtion that generates random color and sticks it to the background per click;
+    var r = Math.floor((Math.random() * 256));
+    var g = Math.floor((Math.random() * 256));
+    var b = Math.floor((Math.random() * 256));
+    console.log(r,g,b);
+    var color = 'rgb'+'('+r+','+g+','+b+')';
+    console.log(color);
+    document.body.style.backgroundColor = color;
+  var body = document.getElementsByTagName("body");
+  console.log(body[0]);
+  body[0].style.backgroundColor = color;
+});
+
+
+$("#backbutton").click(function() {
+  //write funtion that generates random color and sticks it to the background per click;
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  console.log(r, g, b);
+  var color = "rgb" + "(" + r + "," + g + "," + b + ")";
+  console.log(color);
+  $('body').css('background-color', color);
+});
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
